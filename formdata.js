49cbf0650a61b43b94683b7cdd54a89fa6697fa0
Arlan Jaska <ajaska@berkeley.edu>
2016-10-18 02:45:50 -0700

    Always replace FormData if it is an old version

--- formdata.js
+++ formdata.js
@@ -11,49 +11,57 @@
     if ('FormData' in global && 'set' in global.FormData.prototype)
       return;
 
-		if (!('FormData' in global)) {
-			function FormData(form) {
-				this._data = [];
-				if (!form) return;
-				for (var i = 0; i < form.elements.length; ++i) {
-					var element = form.elements[i];
-					if (element.name !== '')
-						this.append(element.name, element.value);
-				}
-			}
+    function FormData(form) {
+      this._data = [];
+      if (!form) return;
+      for (var i = 0; i < form.elements.length; ++i) {
+        var element = form.elements[i];
+        if (element.name !== '')
+          this.append(element.name, element.value);
+      }
+    }
 
-			FormData.prototype = {
-				append: function(name, value /*, filename */) {
-					if ('Blob' in global && value instanceof global.Blob)
-						throw TypeError("Blob not supported");
-					name = String(name);
-					this._data.push([name, value]);
-				},
+    FormData.prototype = {
+      append: function(name, value /*, filename */) {
+        if ('Blob' in global && value instanceof global.Blob)
+          throw TypeError("Blob not supported");
+        name = String(name);
+        this._data.push([name, value]);
+      },
 
-				toString: function() {
-					return this._data.map(function(pair) {
-						return encodeURIComponent(pair[0]) + '=' + encodeURIComponent(pair[1]);
-					}).join('&');
-				}
-			};
-		} else {
-			var FormData = global.FormData;
-		}
-		
+      toString: function() {
+        return this._data.map(function(pair) {
+          return encodeURIComponent(pair[0]) + '=' + encodeURIComponent(pair[1]);
+        }).join('&');
+      }
+    };
+
     /* TODO:
      * get(), getAll(), has() 
      * entries(), keys(), values(), and support of for...of
      */
 
-		FormData.prototype.set = function(name, value) {
+    FormData.prototype.set = function(name, value) {
+      var didSet = false;
+
       for (var i = 0; i < this._data.length; ++i) {
         if (this._data[i][0] === name) {
-          this._data[i][1] = value;
+          if (didSet) {
+            this._data.splice(i, 1);
+            --i;
+          } else {
+            this._data[i][1] = value;
+            didSet = true;
+          }
         }
+      }
+
+      if (!didSet) {
+        this.append(name, value);
       }
     }
 
-		FormData.prototype.delete = function(name) {
+    FormData.prototype.delete = function(name) {
       for (var i = 0; i < this._data.length; ++i) {
         if (this._data[i][0] === name) {
           this._data.splice(i, 1);
